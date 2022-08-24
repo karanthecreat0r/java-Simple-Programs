7995a6afd0a7029e04f5cbe2e7ba7ce29c8e26f1
Karan <49363662+karansparrow@users.noreply.github.com>
2022-08-24 06:53:57 +0530

    Update FINDING_SECOND_LARGEST_NUMBER_OF_3_NUMBERS.java

--- FINDING_SECOND_LARGEST_NUMBER_OF_3_NUMBERS.java
+++ FINDING_SECOND_LARGEST_NUMBER_OF_3_NUMBERS.java
@@ -9,24 +9,15 @@ class SecondLargest
     int b =sc.nextInt();
     System.out.println("Enter c =");
     int c=sc.nextInt();
-//Finding if a is greater than b&c
-    if (a>b&&a>c) {
-//finding if b is grater than c is no c is printed
-      if (b>c) {
-        System.out.println(b+ " Is the Second greates");
-      }
-      else {
-        System.out.println(c+ " Is the Second greates");
-      }
-    }
-//Finding a is lesser than b & c
-    if (a<c&&a<c) {
-      if (b>a) {
-        System.out.println(b+ " Is the Second greates");
-      }
-      else {
-        System.out.println(a+ " Is the Second greates");
-      }
+//Finding if a is greater 2nd
+if (a>b&&a<c||a>c&&a<b) {
+  System.out.println(a);
+}
+  else if (b>a&&b<c||b>c&&b<a) {
+    System.out.println(b);
+  }
+    else {
+      System.out.println(c);
     }
   }
 }
